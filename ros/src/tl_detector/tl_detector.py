f0cd2a412c75b962c1dd1f892a15901cc358a342
Albert Zheng <albert.zheng@zee.aero>
2018-08-20 09:13:31 -0700

    Fix

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -178,13 +178,13 @@ class TLDetector(object):
                         
 	    img_type = "bgr8"
 
-	    if not(self.is_site):
-		img_type = "rgb8"
+	    #if not(self.is_site):
+		#img_type = "rgb8"
 
-            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, img_type)
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, img_type)
     
-            #Get classification
-            return self.light_classifier.get_classification(cv_image)            
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)            
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
